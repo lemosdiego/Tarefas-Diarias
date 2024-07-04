ce28e1dcbba3eabdc6239e8e1b72b8e58628cace
Washington <washington.contato1327@gmail.com>
2024-07-03 23:15:52 -0300

    Alterando e atualizando o projeto

--- js/script.js
+++ js/script.js
@@ -1,121 +1,69 @@
-// Seleção de elementos
-const todoForm = document.querySelector("#todo-form");  
-const todoInput = document.querySelector("#todo-input");  
-const todoList = document.querySelector("#todo-list");  
-const editForm = document.querySelector("#edit-form");  
-const editInput = document.querySelector("#edit-input");  
-const cancelEditBtn = document.querySelector("#cancel-edit-btn"); 
+const botao = document.querySelector(".add")
+const input = document.querySelector(".input")
+const tarefas = document.querySelector(".lista-tarefas")
 
-let oldInputValue;
+let listaDeItens = []
 
-
-//Funções
-const saveTodo = (text) => {
-
-    const todo = document.createElement("div");
-    todo.classList.add("todo");
-
-    const todoTitle = document.createElement ("h3");
-    todoTitle.innerText = text;
-    todo.appendChild(todoTitle);
-
-    const doneBtn = document.createElement("button");
-    doneBtn.classList.add("finish-todo");
-    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>'
-    todo.appendChild(doneBtn);
-
-    const editBtn = document.createElement("button");
-    editBtn.classList.add("edit-todo");
-    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>'
-    todo.appendChild(editBtn);
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("remove-todo");
-    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>'
-    todo.appendChild(deleteBtn);
-
-
-    todoList.appendChild(todo);
-
-    todoInput.value = "";
-    todoInput.focus();
-
-};
-
-const toggleForms = () => {
-    editForm.classList.toggle("hide");
-    todoForm.classList.toggle("hide");
-    todoList.classList.toggle("hide");
-}
-
-const updateTodo = (text) => {
-    const todos = document.querySelectorAll(".todo")
-
-    todos.forEach((todo) => {
-        
-        let todoTitle = todo.querySelector("h3")
-
-        if(todoTitle.innerText === oldInputValue) {
-            todoTitle.innerText = text;
-
-        }
+function adicionarTarefa() {
+    listaDeItens.push({
+        tarefa: input.value,
+        concluida: false
     })
+
+    input.value = ''
+
+    novasTarefas()
 }
 
-//Eventos
-todoForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-
-    const inputValue = todoInput.value;
-
-    if (inputValue) {
-       saveTodo(inputValue)
-    }
-});
+function novasTarefas() {
+    let novaTarefa = ''
 
 
-document.addEventListener("click", (e) => {
-   
-    const targetEl = e.target;
-    const parentEl = targetEl.closest("div");
-    let todoTitle;
+    listaDeItens.forEach((tarefa, index) => {
+        novaTarefa = novaTarefa + `
+        <li class="${tarefa.concluida && "feito"}">
+            <div class="tarefa">
+                <p>${tarefa.tarefa}</p>
+            </div>
+            <div class="itens  ${tarefa.concluida && "feito"}">
+                <button onclick="tarefaConcluida(${index})">feita</button>
+                <button  onclick="removerTarefa(${index})">remover</button">
+            </div>
+         </li>
+        
+        `
+    })
 
-    if (parentEl && parentEl.querySelector("h3")) {
-        todoTitle = parentEl.querySelector("h3").innerText;
-    }
+    tarefas.innerHTML = novaTarefa
+
+    localStorage.setItem('lista', JSON.stringify(listaDeItens))
+}
+
+function removerTarefa(index) {
+
+    listaDeItens.splice(index, 1)
+
+    novasTarefas()
+}
+
+function tarefaConcluida(index) {
+
+    listaDeItens[index].concluida = !listaDeItens[index].concluida
+
+    novasTarefas()
+}
+
+function recarregarLista() {
+    const localStorageTarefas = localStorage.getItem('lista')
 
 
-    if (targetEl.classList.contains("finish-todo")) {
-    parentEl.classList.toggle("done");
-    }
-
-    if (targetEl.classList.contains("remove-todo")) {
-        parentEl.remove();
-    }
-    
-    if (targetEl.classList.contains("edit-todo")) {
-        toggleForms();
-
-        editInput.value = todoTitle;
-        oldInputValue = todoTitle;
-    }
-});
-
-cancelEditBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-
-    toggleForms();
-
-});
-
-editForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    const editInputValue = editInput.value
-
-    if (editInputValue) {
-       updateTodo(editInputValue)
+    if (localStorageTarefas) {
+        listaDeItens = JSON.parse(localStorageTarefas)
     }
 
-    toggleForms()
-})
+    novasTarefas()
+}
+
+
+recarregarLista()
+botao.addEventListener('click', adicionarTarefa)
